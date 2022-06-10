dcfab10561dd9f566c847b60213b0eca75d942c8
gopikrishnangs44 <59282012+gopikrishnangs44@users.noreply.github.com>
2022-06-10 13:22:50 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools 
+
+setuptools.setup(
+    name='pyINSAT',
+    version='0.0.1',
+    author='Gopikrishnan G S',
+    distribution='Python wrapper for INSAT DATA',
+    packages=['pyINSAT'],
+    website=['www.atmoslabiitkgp.com']
+
+)
